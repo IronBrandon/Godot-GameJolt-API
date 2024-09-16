007a75e56c397cb73d1709498eaea23e5c092a4d
IronBrandon <IronBrandon22@gmail.com>
2024-09-16 13:07:33 -0500

    Update main.gd and README.md
    Added more documentation with improved readability, removed several deprecated methods, and more.

--- main.gd
+++ main.gd
@@ -14,7 +14,7 @@ class_name GameJoltAPI extends HTTPRequest
 
 ## A simple GameJolt API node.
 ## 
-## Credits: Ackens, Piotr Rojewski, Deakcor, and IrønBrandon.[br][br]
+## [b]Credits[/b]: Ackens, Piotr Rojewski, Deakcor, and IrønBrandon.[br][br]
 ## 
 ## Also look at the [GameJoltAPI.Request] class.[br][br]
 ## 
@@ -39,10 +39,11 @@ const BASE_GAMEJOLT_API_URL = 'https://api.gamejolt.com/api/game/v1_2'
 @export var private_key: String ## Your game's private key.
 @export var game_id: String ## Your game's ID. This is public, just look at your game's URL and copy the numbers.
 @export var auto_batch: bool = true ## Merge queued requests in one batch.
-@export var verbose: bool = false ## Prints more text.
+@export var auto_auth_in_ready: bool = false ## Automatically calls the [method user_auto_auth] method during ready.
+@export var verbose: bool = false ## Causes funtions to print detailed text.
 
-var _username_cache: String ## The currently cached username.
-var _token_cache: String ## The currently cached token.
+var _username_cache: String
+var _user_token_cache: String
 var busy: bool ## If true the GameJoltAPI is currently busy with a GJ call.
 var queue: Array[Request] = [] ## The current queue of [param Request]s.
 var current_request: Request ## The currently active GameJolt request.
@@ -61,10 +62,16 @@ class Request:
 func _init() -> void:
 	request_completed.connect(_on_HTTPRequest_request_completed)
 
-func _call_gj_api(type: String, parameters: Dictionary = {}, sub_types: Array = []) -> void:
+func _ready() -> void:
+	if auto_auth_in_ready:
+		user_auto_auth()
+
+func _call_gj_api(type: String, parameters: Dictionary = {}, sub_types: Array = [], include_user: bool = true) -> void:
 	var request_error: Error = OK
-	parameters["username"] = parameters.get("username", _username_cache)
-	parameters["user_token"] = parameters.get("user_token", _token_cache)
+	if include_user:
+		parameters["username"] = parameters.get("username", _username_cache)
+		parameters["user_token"] = parameters.get("user_token", _user_token_cache)
+	else: parameters.erase("username"); parameters.erase("user_token")
 	if busy:
 		request_error = ERR_BUSY
 		if auto_batch and type != '/batch/':
@@ -140,7 +147,7 @@ func _verbose(message) -> void:
 
 
 func get_username() -> String: return _username_cache ## Returns the username cache.
-func get_token() -> String: return _token_cache ## Returns the user token cache.
+func get_token() -> String: return _user_token_cache ## Returns the user token cache.
 func time_fetch(): _call_gj_api('/time/',{}) ## Fetches the current time.
 
 func batch_request(requests: Array[Request], parallel: bool = true, break_on_error: bool = false): ## Calls a batch of [GameJoltAPI.Request]s.
@@ -152,7 +159,6 @@ func batch_request(requests: Array[Request], parallel: bool = true, break_on_err
 	_call_gj_api('/batch/',{requests = requests, parallel = parallel, break_on_error = break_on_error}, sub_types)
 
 #region USERS
-
 ## Attempts to automatically authenticate the user with the URL in Web exports.
 ## [br]When debugging, you can add this to the URL: [codeblock lang=text]?gjapi_username=<yourusername>&gjapi_token=<yourtoken>[/codeblock] Request type is "/users/auth/".
 func user_auto_auth() -> void:
@@ -162,16 +168,15 @@ func user_auto_auth() -> void:
 		_username_cache = tmp
 		tmp = JavaScriptBridge.eval('urlParams.get("gjapi_token")', true)
 		if tmp is String:
-			_token_cache = tmp
-			_call_gj_api('/users/auth/', {user_token = _token_cache, username = _username_cache})
+			_user_token_cache = tmp
+			_call_gj_api('/users/auth/')
 
-## Attempts to authenticate a user with the given [param username] and [param token]. [br][br]Request type is "/users/auth/".
+## Attempts to authenticate a user with the given [param username] and [param token].[br][b]NOTE[/b]: The username and token cache are updated here, not after authentication.[br][br]Request type is "/users/auth/".
 func user_auth(username: String, token: String) -> void:
-	_call_gj_api('/users/auth/',
-		{user_token = token, username = username})
-	_username_cache = username; _token_cache = token
+	_username_cache = username; _user_token_cache = token;
+	_call_gj_api('/users/auth/')
 
-func user_fetch(username: String, id: int=0) -> void: ## Fetches user data.[br][br]Request type is "/users/"
+func user_fetch(username: String, id: int=0) -> void: ## Fetches the data of either the [param username] or user [param id] data.[br][br]Request type is "/users/"
 	_call_gj_api('/users/', {username = username, user_id = id})
 
 func user_friends_fetch() -> void: ## Fetches the currently cached user's friends list. [br][br]Request type is "/friends/"
@@ -179,7 +184,6 @@ func user_friends_fetch() -> void: ## Fetches the currently cached user's friend
 #endregion
 
 #region SESSIONS
-
 func session_open(): ## Opens a session. [br][br]Request type is "/sessions/open/"
 	_call_gj_api('/sessions/open/')
 
@@ -194,65 +198,47 @@ func session_check(): ## Checks for an active session. [br][br]Request type is "
 #endregion
 
 #region SCORES
+## Fetches a list of scores for cached user, a [param guest] or [param global]ly.[br]Leave [param guest] empty if you want global or cached user. [br][br]Request type is "/scores/"
+func scores_fetch(global=false, guest: String = "", limit=null, table_id=null, better_than=null, worse_than=null) -> void:
+	var parameters = {limit = limit, table_id = table_id, better_than = better_than, worse_than = worse_than}
+	if !guest.is_empty(): parameters["guest"] = guest
+	_call_gj_api('/scores/', parameters, [], !global && guest.is_empty())
 
-func scores_fetch(table_id=null, limit=null, better_than=null, worse_than=null):
-	_call_gj_api('/scores/',
-		{limit = limit, table_id = table_id, better_than = better_than, worse_than = worse_than})
-
-func scores_fetch_guest(guest, limit=null, table_id=null, better_than=null, worse_than=null):
-	_call_gj_api('/scores/',
-		{guest = guest, limit = limit, table_id = table_id, better_than = better_than, worse_than = worse_than})
-
-func scores_fetch_global(limit=null, table_id=null, better_than=null, worse_than=null):
-	_call_gj_api('/scores/',
-		{username = null, user_token = null, limit = limit, table_id = table_id, better_than = better_than, worse_than = worse_than})
-
-## Attempts to add a score to the cached user or guest.
-func scores_add(score, sort, guest: String = "", table_id=null):
-	var parameters = {score = score, sort = sort, table_id = table_id}
-	if _username_cache && _token_cache:
-		parameters["username"] = _username_cache; parameters["user_token"] = _token_cache
-	else:
-		parameters["guest"] = guest
+## Attempts to add a score to the cached user or guest.[br]Leave [param guest] empty to use the cached user ([/i]User must be authenticated[/i]).
+func scores_add(score_string, sort_number, guest: String = "", table_id=null) -> void:
+	var parameters = {score = score_string, sort = sort_number, table_id = table_id}
+	if !guest.is_empty(): parameters["guest"] = guest
 	# Attempt call
 	if ((parameters["username"] && parameters["user_token"])\
 		|| parameters["guest"]):
-			_call_gj_api('/scores/add/', parameters)
+			_call_gj_api('/scores/add/', parameters, [], guest.is_empty())
 
-## Attempts to add a score to a guest.
-## @deprecated: Add a guest name in [method scores_add] instead.
-func scores_add_guest(score, sort, guest, table_id=null):
-	_call_gj_api('/scores/add/',
-		{score = score, sort = sort, guest = guest, table_id = table_id})
+func scores_fetch_rank(sort, table_id=null) -> void: ## Returns the rank of a score on a table. [br][br]Request type is "/scores/get-rank/"
+	_call_gj_api('/scores/get-rank/', {sort = sort, table_id = table_id})
 
-func scores_fetch_rank(sort, table_id=null):
-	_call_gj_api('/scores/get_rank/', {sort = sort, table_id = table_id})
-
-func scores_fetch_tables():
+func scores_fetch_tables() -> void: ## Fetches a list of high-score tables. [br][br]Request type is "/scores/tables/"
 	_call_gj_api('/scores/tables/',{})
 #endregion
 
 #region TROPHIES
-
 ## Fetches trophies.[br][br]If you want a specific trophy or set of trophies, pass them through [param trophy_ids] as '123456,135792' each separated by commas.
-## [br][br]If you want only achieved or unachieved trophies, pass [param achieved] as 'true' or 'false'.
+## [br]If you want only achieved or unachieved trophies, pass [param achieved] as 'true' or 'false'.
 func trophy_fetch(trophy_ids: String, achieved: String = '') -> void:
 	if _username_cache != null: _call_gj_api('/trophies/',
 		{achieved = achieved, trophy_id = trophy_ids})
 
-## Unlocks a new trophy for the authenticated user. [i]User must be authenticated[/i]
+## Unlocks the trophy with id [param trophy_id] for the authenticated user. [i]User must be authenticated[/i] [br][br]Request type is "/trophies/remove-achieved/"
 func trophy_add_achieved(trophy_id) -> void:
 	if _username_cache != null: _call_gj_api('/trophies/add-achieved/',
 		{trophy_id = trophy_id})
 
-## Removes a trophy from the authenticated user. [i]User must be authenticated[/i]
+## Removes the trophy with id [param trophy_id] from the authenticated user. [i]User must be authenticated[/i][br][b]NOTE[/b]: Only for debugging. [br][br]Request type is "/trophies/remove-achieved/"
 func trophy_remove_achieved(trophy_id) -> void:
 	if _username_cache != null: _call_gj_api('/trophies/remove-achieved/',
 		{trophy_id = trophy_id})
 #endregion
 
 #region DATA STORE
-
 func data_fetch(key, global=true):
 	if global:
 		_call_gj_api('/data-store/', {username = null, user_token = null, key = key})
